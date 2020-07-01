b041ea7ac7c3b8b94d8cb58dd2cb9c11f08083f2
eyebyeye <pendejo0725@gmail.com>
2020-07-01 10:15:44 -0400

    maqueta

---
+++ Vmaqueta2.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"><title>Buscamed</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+ <script type="text/javascript" src="C:\xampp\htdocs\maquetacion\maquetacion/jquery.js"></script>
+ <!-- <style type="text/css" src="asset/stilo.css"></style> -->
+ <!-- <link rel="stylesheet" type="text/css" href="asset/stilo.css"> -->
+ <link rel="stylesheet" type="text/css" href="asset/stilo3.css">
+ <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Rubik">
+</head>
+<body class="">
+
+
+	<nav class="navbar navbar-inverse navs ">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#"><h2 class="buscamed" ><b>Buscamed.do</b></h2></a>
+		    </div>
+		    
+		    <ul class="nav navbar-nav navbar-right">
+		      <li><a href="#"><button class="btn btn-primary entrar" >Entrar</button></a></li>
+		      <li><a href="#"><button class="btn btn-primary registrarse" ><p  class="textRegistrarse">Registrarse</p></button></a></li>
+		    </ul>
+		  </div>
+		</nav>
+          
+          <br><br>
+
+<br><br><br><br>
+
+		<div class="container-fluid" style="">	
+
+		  <div class="row">
+
+			<div class="col-md-4 col-sm-12 col-xs-12" > 
+				<span style="margin-left: 190px"><img src="asset/primera.jpg" class="image" width="200px"  style="margin-top: 40px"></span>
+			</div>
+
+
+			<div class="col-md-8 col-sm-12 col-xs-12 borderLeft"  >
+				
+				<div class="" >
+					  
+					  <div class="row">
+					  	<div class="col-md-8 docame" ><b>DOCAMED</b></h4 >
+					  		<h3 class="textD"><p>C. García Godoy #9, Gazcue. y Enrique Henriquez #210, Gazcue,<p> Distrito Nacional Distrito Nacional, República Dominicana 10101</p></p></h3 >
+					  		<div ><h4 class="textD"><b>Télefono:<a href="#">8096882929</a></b></h4 ></div>
+					  		<div ><h4 class="textD "><b>Email:<a href="#">contacto@docamed.com</a></b></h4 ></div>
+
+					  		<button class="btn btn-primary buttonImage"  type="submit">
+							        <img style="" src="asset/galeria.png" width="30px"  height="30px"><span >Galeria</span>
+
+							      </button>
+
+					  	</div>
+					  	<div class="col-md-4"></div>
+					  </div>
+
+					   
+
+			</div>
+</div>
+</div>
+
+<br><!-- 
+		<nav class="nav2">
+
+
+			<div >
+				<div class="dropdown marginDrod1" >
+						 <button  class="btn btn-light dropdown-toggle drodown1" type="button" data-toggle="dropdown"><span class="textDro" >Seleccione la especialidad</span>
+					      <b> <span class="caret"></b></span></button>
+						 <ul class="dropdown-menu">
+								 <li><a href="#">HTML</a></li>
+							  <li><a href="#">CSS</a></li>
+								 <li><a href="#">JavaScript</a></li>
+							</ul>
+					</div>
+			</div>
+
+			<div style="flex-wrap: wrap;" >
+				<input type="text"  class="form-control drodown2 marginDrod2" placeholder="Hacer una busqueda" placeholder="color:red" name="">
+			</div>
+		</nav> -->
+
+
+			<nav class="nav2">
+
+                    <div class="row">
+
+                    	<div class="col-md-6">
+                    		<div class="dropdown marginDrod1" >
+						 <button  class="btn btn-light dropdown-toggle drodown1" type="button" data-toggle="dropdown"><span class="textDro" >Seleccione la especialidad</span>
+					      <b> <span class="caret"></b></span></button>
+						 <ul class="dropdown-menu">
+								 <li><a href="#">HTML</a></li>
+							  <li><a href="#">CSS</a></li>
+								 <li><a href="#">JavaScript</a></li>
+							</ul>
+					</div>
+                    	</div>
+
+                    	<div class="col-md-6">
+                    		<input type="text"  class="form-control drodown2 marginDrod2" placeholder="Hacer una busqueda" placeholder="color:red" name="">
+                    	</div>
+
+                    </div>
+		
+	     	</nav>
+
+	     	<br>
+	     	<br>
+
+	     	<div class="container-fluid">
+	     		
+               
+                 <div class="row" style="display: flex">
+
+                 	<div class="col-md-6 col-sm-12" style="flex-wrap: wrap;" >
+                 		<div class="card borderSombras tamanoCUa" >
+			                 			<div class="card-body"   >
+			                 				 <div class="row">
+			                 				 	<div class="col-md-4">
+			                 				 		<img style="margin-left: 30px;margin-bottom: 30px" src="asset/segunda.jpg" height="120px" width="120px">
+			                 				 	</div>
+
+			                 				 	<div class="col-md-8 col-sm-12 col-xs-6" >
+			                 				 		<h5><b><a href="#"  class="textAzul" >Lucrecia Amparo Vasquez</a></b></h5>
+			                 				 		<h5 class="textNegr" >Gastroenterólogo</h5>
+			                 				 		<h5  class="textNegr">Distrito Nacional</h5>
+			                 				 	</div>
+			                 				 </div>
+			                 			</div>
+			                 			
+			                 			<div class="card-footer" style="border-top:	solid 1px #e0ebeb">
+			                 				  <div class="row">
+			                 				  	<div class="col-md-4 textNegr"><center ><h4>Hacer cita</h4></center></div>
+			                 				  	<div class="col-md-4 textNegr"><center ><h4>Ver Telefonos</h4></center></div>
+			                 				  	<div class="col-md-4 textNegr"><center ><h4>Ver perfil</h4></center></div>
+			                 				  </div>
+			                 			</div>
+			                 		</div>
+                 	</div>
+
+
+                 	<div class="col-md-6 col-sm-12"  style="flex-wrap: wrap;">
+                 		   <div class="row">
+			                 		 	<div class="col-md-12 borderSombras tamanoCUa">
+			                 		 		<h2 class="textAzul" style="margin-top: 10%;margin-left: 15px"><b>Acerca de</b></h2>
+			                 		 		<p></p>
+			                 		 		<p class="textNegr" style="margin-top: 3%;margin-left: 15px">Somos un complejo sanitario que brinda servicios de salud a través de unidades médicas y consultorios independientes, permitiendo el desarrollo competitivo a beneficio de los pacientes, destacando en excelencia, instalaciones a la vanguardia y equipos de última tecnología.</p>
+			                 		 	</div>
+			                 		 </div>
+                 	</div>
+
+
+                 </div>
+
+	     	</div>
+
+
+
+	     	<br>
+	     	
+	     	<!-- //////////////// -->
+
+	     	<div class="row" style="display: flex">
+
+	     		<div class="col-md-6" style="flex-wrap: wrap;">
+	     			<div class="card borderSombras tamanoCUa" style="margin-left: 45px"  >
+			                 			<div class="card-body"  >
+			                 				 <div class="row">
+			                 				 	<div class="col-md-4">
+			                 				 		<img style="margin-left: 30px;margin-bottom: 30px" src="asset/segunda.jpg" height="120px" width="120px">
+			                 				 	</div>
+
+			                 				 	<div class="col-md-8" >
+			                 				 		<h5><b><a href="#"  class="textAzul"  >Ramon Antonio Almodovar Hache</a></b></h5>
+			                 				 		<h5 class="textNegr" >Alergólogo</h5>
+			                 				 		<h5 class=" textNegr" >Distrito Nacional</h5>
+			                 				 	</div>
+			                 				 </div>
+			                 			</div>
+			                 			
+			                 			<div class="card-footer" style="border-top:	solid 1px #e0ebeb" >
+			                 				  <div class="row">
+			                 				  	<div class="col-md-4 textNegr"><center ><h4>Hacer cita</h4></center></div>
+			                 				  	<div class="col-md-4 textNegr"><center ><h4>Ver Telefonos</h4></center></div>
+			                 				  	<div class="col-md-4 textNegr"><center ><h4>Ver perfil</h4></center></div>
+			                 				  </div>
+			                 			</div>
+			                 		</div>
+	     		</div>
+
+	     		<div class="col-md-6" style="margin-right: 30px" style="flex-wrap: wrap;">
+
+	     			  <div class="row borderSombras tamanoCUa">
+	     			  	<div class="col-md-6 ">
+	     			  		  
+	     			  		  <h2 class="textAzul"><b>Especialidades</b></h2>
+			                 		 		 <ul class="textNegr">
+			                 		 		 	<li>Alergólogo</li>
+			                 		 		 	<li>Cardiólogo</li>
+			                 		 		 	<li>Cirugía General</li>
+			                 		 		 	<li>Cirugía Torácica</li>
+			                 		 		 	<li>Cirujano Oncólogo</li>
+			                 		 		 	<li>Gastroenterólogo</li>
+			                 		 		 	<li>Ginecólogo</li>
+			                 		 		 	<li>Pediatra</li>
+			                 		 		 	<li>Perinatólogo</li>
+			                 		 		 </ul >	     			  	</div>
+	     			  	<div class="col-md-6" style="flex-wrap: wrap;">
+	     			  		 <ul style="margin-top: 50px" class="textNegr">
+			                 		 		 	<li>Anestesiólogo</li>
+			                 		 		 	<li>Cardiólogo Intensivista</li>
+			                 		 		 	<li>Cirugía Oncológica</li>
+			                 		 		 	<li>Cirujano Gastroenterólogo</li>
+			                 		 		 	<li>Gastroenterólogo Endoscopista</li>
+			                 		 		 	<li>Ginecobstetra Infertilidad Y Fertilidad</li>
+			                 		 		 	<li>Oncólogo</li>
+			                 		 		 	<li>Pediatra Perinatal Y Neonatología</li>
+			                 		 		 	<li>Psicólogo</li>
+			                 		 		 </ul>
+	     			  	</div>
+	     			  </div>
+	     			
+
+	     		</div>
+
+	     	</div>
+			
+</body>
+</html>
